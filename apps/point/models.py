2f68e1cbc2f3cee85029124cd4ab43fd31ba0a39
Andrey Doroshenko <dorosshh@gmail.com>
2010-09-30 14:36:50 +0300

    создана админка

--- apps/point/models.py
+++ apps/point/models.py
@@ -5,17 +5,38 @@ from django.db import models
 class Transport(models.Model):
     name = models.CharField(max_length=155)
     ico = models.ImageField(upload_to="ico_transport")
-    price = models.IntegerField()
+    price = models.FloatField()
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.name
 
 class Route(models.Model):
-    transport = models.ForeignKey(Transport)
+    transport_type = models.ForeignKey(Transport)
     route = models.CharField(max_length=155)
-    interval = models.IntegerField()
-    speed = models.IntegerField()
-    price = models.IntegerField()
+    interval = models.FloatField()
+    speed = models.FloatField()
+    price = models.FloatField() 
+    one_pay = models.BooleanField()
 
-class Point(models.Model):
+    def __unicode__(self):
+        return self.route
+
+class Metastation(models.Model):
+    name = models.CharField(max_length=55)
+    address = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+class Station(models.Model):
     route = models.ForeignKey(Route)
     name = models.CharField(max_length=155)
-    coordinate_x = models.IntegerField()
-    coordinate_y = models.IntegerField()
+    coordinate_x = models.FloatField()
+    coordinate_y = models.FloatField()
+    next_station = models.CharField(max_length=155)
+    prev_station = models.CharField(max_length=155)
+    meta_station = models.ForeignKey(Metastation)
+
+    def __unicode__(self):
+        return self.name
